89cd5b9359a6f94968b8d22b4265ee1c22b686c5
Helderfox49 <mbottoguyzo2012@gmail.com>
2026-01-18 08:07:22 +0100

    Challenge of the day 18/01/2026 in python Free Shipping

---
+++ 18_01_2026_free_shipping/8_01_2026_free_shipping.py
@@ -0,0 +1,25 @@
+def gets_free_shipping(cart, minimum):
+    prices = {
+        "shirt": 34.25,
+        "jeans": 48.50,
+        "shoes": 75.00,
+        "hat": 19.95,
+        "socks": 15.00,
+        "jacket": 109.95
+    }
+
+    total = sum(prices[item] for item in prices)
+    
+    if total < minimum:
+        return False
+    else:
+        return True
+
+
+if __name__ == "__main__":
+    print(gets_free_shipping(["shoes"], 50)) # should return True.
+    print(gets_free_shipping(["hat", "socks"], 50)) # should return False.
+    print(gets_free_shipping(["jeans", "shirt", "jacket"], 75)) # should return True.
+    print(gets_free_shipping(["socks", "socks", "hat"], 75)) # should return False.
+    print(gets_free_shipping(["shirt", "shirt", "jeans", "socks"], 100)) # should return True.
+    print(gets_free_shipping(["hat", "socks", "hat", "jeans", "shoes", "hat"], 200)) # should return False
